87990b3d15898a46f1cb1977e3f733b3518fb3ca
alencartha <alencar_tha@hotmail.com>
2022-01-10 08:28:48 -0300

    finalização projeto tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -22,11 +22,26 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = []
 
-  getHeroes():void{
+  getHeroes(): void {
     this.heroService.getHeroes()
-    .subscribe(heroes => {
-      console.log(heroes)
-      this.heroes = heroes
-    })}
+      .subscribe(heroes => {
+        console.log(heroes)
+        this.heroes = heroes
+      })
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
 
 }
